288f3c531b012bde9625226d8b72890faee78d94
TF2694 <feldmant@uw.edu>
2016-02-17 22:17:14 -0800

    Loading data sets
    Sanitation, Health infrastructure, Literacy Rate

--- CourseProject.m
+++ CourseProject.m
@@ -1,1 +1,7 @@
 %Reading in data
+%http://apps.who.int/gho/data/node.main.46?lang=en
+SanitationData = readtable('SanitationData.xlsx');
+%http://apps.who.int/gho/data/node.main.A1444?lang=en
+HealthInfrastructure = readtable('HealthInfrastructure.xlsx');
+%http://data.worldbank.org/indicator/SE.ADT.LITR.ZS
+LiteracyRate = readtable('LiteractyRate.xls');
