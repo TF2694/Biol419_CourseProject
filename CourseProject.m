9759a0972ec38282fc42a1e068fb7be978001835
TF2694 <feldmant@uw.edu>
2016-02-18 10:53:34 -0800

    Loading more data
    Population, Sanitation,GNI

--- CourseProject.m
+++ CourseProject.m
@@ -1,7 +1,11 @@
 %Reading in data
-%http://apps.who.int/gho/data/node.main.46?lang=en
-SanitationData = readtable('SanitationData.xlsx');
+%http://data.worldbank.org/indicator/SH.STA.ACSN
+SanitationData = readtable('SanitationData.xls');
 %http://apps.who.int/gho/data/node.main.A1444?lang=en
 HealthInfrastructure = readtable('HealthInfrastructure.xlsx');
 %http://data.worldbank.org/indicator/SE.ADT.LITR.ZS
 LiteracyRate = readtable('LiteractyRate.xls');
+%http://data.worldbank.org/indicator/SP.POP.TOTL
+PopulationData = readtable('PopulationData.xls');
+%http://data.worldbank.org/indicator/NY.GNP.ATLS.CD
+GniData = readtable('GNIData.xls');
